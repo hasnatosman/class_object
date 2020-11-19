f1b1ffe247cd2a712c72ec19e9d880979a55986a
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2020-11-19 23:49:25 +0600

    Add files via upload

---
+++ square_shape.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.speed(5)
+turtle.color("blue")
+
+
+def draw_square(side_length):
+    for i in range(4):
+        turtle.forward(side_length)
+        turtle.left(90)
+
+
+counter = 0
+while counter < 72:
+    draw_square(100)
+    turtle.right(5)
+    counter += 1
+
+turtle.onclick()
+turtle.hideturtle()
