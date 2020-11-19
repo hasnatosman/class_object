c428edc2e4e7b8d2bf9614b6cd1c5a9b61971538
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2020-11-19 23:48:44 +0600

    Add files via upload

---
+++ class_instance.py
@@ -0,0 +1,23 @@
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+emp_1 = Employee('Hasnat', 'Osman', 50000)
+emp_2 = Employee('Test', 'User', 60000)
+
+print(emp_1.fullname())
+print(emp_1.pay)
+print(emp_1.email)
+# print(Employee.fullname(emp_1))
+
+print(emp_2.fullname())
+print(emp_2.pay)
+print(emp_2.email)
+# print(Employee.fullname(emp_2))
