3b37ac7d319e51d98b62cab059a7c29b9e0128d0
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2020-11-24 19:46:17 +0600

    Add files via upload

---
+++ vehicle.py
@@ -0,0 +1,17 @@
+class Vehicle:
+    name = ''
+    color = ''
+
+    def start():
+        print("Starting the car.")
+
+    def brake():
+        print("Brake the car.")
+
+    def turn():
+        print("Turn the car.")
+
+
+my_car = Vehicle()
+my_car.name = 'Hasnat'
+print(my_car.name)
